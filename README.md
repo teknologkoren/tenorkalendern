016e55322a216cb6638a8ef9a54c87a0e4ddea15
Martin Alge <martin@alge.se>
2020-11-20 18:49:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tenorkalendern
+Ett projekt med ett gäng knölar gjort av ett gäng Teno... Knölar
+
+## Setup
+1: Create a virtual environment with
+´´´virtualenv env -p python3´´´
+2: activate it with:
+´´´source env/bin/activate´´´
+3: install the pip requirements
+´´´pip install -r requirements.txt´´´
+run the flask rebug app
+´´´python app.py´´´
---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, send_from_directory, render_template, redirect
+import os
+from os import path
+
+extra_dirs = ['static']
+extra_files = extra_dirs[:]
+for extra_dir in extra_dirs:
+    for dirname, dirs, files in os.walk(extra_dir):
+        for filename in files:
+            filename = path.join(dirname, filename)
+            if path.isfile(filename):
+                extra_files.append(filename)
+
+
+app = Flask(__name__)
+
+@app.context_processor
+def inject_debug():
+    return dict(debug=app.debug)
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
+
+@app.route('/favicon.ico')
+def favicon():
+    return redirect("/static/images/favicon.ico")
+
+@app.route('/')
+def index():
+    return redirect("/main")
+
+@app.route('/<pagename>')
+def page(pagename):
+    pagename = "pages/{}.html".format(pagename)
+    return render_template("page.html", page = pagename)
+
+
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(extra_files=extra_files, debug=True, host="0.0.0.0")
+

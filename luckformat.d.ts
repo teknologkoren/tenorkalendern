721d4cf041dc71879c0f80cd1057de99ae48eb5e
Oskar Segersvärd <victorystick@gmail.comn>
2020-11-21 21:35:35 +0000

    Create an interface definition for the luckformat

---
+++ luckformat.d.ts
@@ -0,0 +1,46 @@
+/** Just does it all, returning an element containing the calendar. */
+declare function createIt(options: {
+    backgroundImageUrl: string,
+    windows: Array<{position: Position, contentUrls?: ContentUrls}>,
+}): HTMLElement;
+
+/** Creates an advent calendar to which windows can be added. */
+declare function createAdventCalendar(options: {
+    container: HTMLElement,
+    backgroundImageUrl: string,
+}): Calendar;
+
+declare interface Calendar {
+    /**
+     * Creates a CalendarWindow at the given `position` with the given
+     * `contentUrls`. If no content is passed, the window is locked and cannot
+     * be opened.
+     *
+     * If an attempt is made to add more than 24 windows, errors will be
+     * thrown.
+     *
+     * Example:
+     *   calendar.addWindow(
+     *       {x: 10, y: 20, width: 100, height: 80}, {imageUrl, linkUrl});
+     */
+    addWindow(position: Position, contentUrls?: ContentUrls): CalendarWindow;
+}
+
+declare interface Position {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+declare interface ContentUrls {
+    /** The url of the image to display inside the window. Will be centered. */
+    imageUrl: string;
+    /** The url that the content image should link to. */
+    linkUrl: string;
+}
+
+declare interface CalendarWindow {
+    /** The calendar day. A number between 1 and 24. Yes, 24. Bite me! */
+    readonly day: number;
+}

555aa45252bcfa88ea4f185f09289c99dedadb09
Martin Alge <alge@46elks.com>
2020-11-29 20:16:21 +0100

    return 404 status code with 404 page

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ app = Flask(__name__)
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html', title = '404')
+    return (render_template('404.html', title = '404'), 404)
 
 @app.context_processor
 def inject_debug():
